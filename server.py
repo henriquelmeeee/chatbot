d0858867ccc234a7adfac38555d3661a14e82d32
Henrique <86432400+henriquelmeeee@users.noreply.github.com>
2022-06-02 14:24:39 -0300

    Atualizando a API
    API atualizada, agora aleatoriza respostas para múltiplas linhas no banco de dados

--- server.py
+++ server.py
@@ -17,11 +17,15 @@ app = Flask(__name__, template_folder=template_dir, static_folder=static_dir)
 import mysql.connector
 from mysql.connector import Error
 mysql = MySQL()
-app.config['MYSQL_DATABASE_USER'] = 'user'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'pass'
-app.config['MYSQL_DATABASE_DB'] = 'database'
-app.config['MYSQL_DATABASE_HOST'] = 'host'
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = '123'
+app.config['MYSQL_DATABASE_DB'] = 'chatbot'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 mysql.init_app(app)
+    
+conn = mysql.connect(); cursor = conn.cursor()
+
+cursor.execute("CREATE TABLE IF NOT EXISTS messages (message text, response text)")
 
 # Checar SQL Injection em strings enviadas pelo usuário
 
@@ -46,26 +50,44 @@ def application():
 # response: resposta da API de uma determinada string.
 # response/new: nova resposta da API de uma determinada string.
 
-@app.route('/api/v1/response/', methods=["POST"])
+@app.route('/api/v1/response/', methods=["POST", "GET"])
 def getresponse():
-    content = request.json
-    if content["message"] is None or content["message"] == '':
-        return jsonify({"error": "The 'message' parameter must contain at least 1 character"})
+    if request.method == "GET":
+        return jsonify({"error": "GET method is not allowed"}), 405
     else:
-        conn = mysql.connect(); cursor = conn.cursor()
-        if not is_sqli(content["message"]):
-            cursor.execute(f"SELECT response FROM messages WHERE message='{content['message']}'")
-            response = str(cursor.fetchall())
-            if response == '()':
-                return jsonify({"error": "No response in the database found"})
-            else:
-                return jsonify({"response": f"{response}"})
+        content = request.json
+        try:
+            value = content["message"]
+        except:
+            return jsonify({"error": "The 'message' parameter was not specified"}), 400
         else:
-            return jsonify({"error": "The 'message' parameter contains invalid characters"})
+            if content["message"] == '':
+                return jsonify({"error": "The 'message' parameter must contain at least 1 character"}), 400
+            else:
+                conn = mysql.connect(); cursor = conn.cursor()
+                if not is_sqli(content["message"]):
+                    cursor.execute(f"SELECT response FROM messages WHERE message='{content['message']}'")
+                    response = cursor.fetchall(); n = -1
+                    for s in response:
+                        try:
+                            response[n]
+                        except:
+                            break
+                        else:
+                            n += 1
+                    try:
+                        response = response[random.randint(0, n)]
+                    except:
+                        return jsonify({"error": "No response in the database found"})
+                    else:
+                        response = str(response).replace('(', '').replace(')', '').replace('\'', '').replace(',', '')
+                        return jsonify({"response": f"{response}"})
+                else:
+                    return jsonify({"error": "The 'message' parameter contains invalid characters"})
 
 
 @app.route('/api/v1/response/new', methods=["POST"])
 def newresponse():
     return ''
 
-app.run(host='0.0.0.0', port=33, debug=True)
+app.run(host='0.0.0.0', port=80)
